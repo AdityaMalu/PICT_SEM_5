325b29d3ce52267944b1dee1367c064134efc94d
Aditya Malu <98596211+AdityaMalu@users.noreply.github.com>
2023-11-17 08:28:26 +0530

    Update Scheduler.py

--- LP 1/OS/Scheduler.py
+++ LP 1/OS/Scheduler.py
@@ -1,139 +1,178 @@
-class Job:
-    def __init__(self, job_id, arrival_time, burst_time, priority):
-        self.job_id = job_id
-        self.arrival_time = arrival_time
-        self.burst_time = burst_time
-        self.remaining_time = burst_time
+from typing import List
+
+class Process:
+    def __init__(self, name, burst, AT, priority=None):
+        self.name = name
+        self.BT = burst
+        self.WT = 0
+        self.AT = AT
+        self.CT = 0
+        self.TAT = 0
+        self.remBT = burst
         self.priority = priority
+        self.flag = False
 
-    def __str__(self):
-        return f"Job {self.job_id}: Arrival Time={self.arrival_time}, Burst Time={self.burst_time}, Priority={self.priority}"
+    def display(self):
+        print(f"{self.name}\t{self.BT}\t{self.AT}\t{self.CT}\t{self.TAT}\t{self.WT}\t{self.priority}")
 
-class Scheduler:
-    def __init__(self):
-        self.queue = []
-        self.time = 0
-        self.avg_waiting_time = 0
-        self.total_burst_time = 0
+def sortByArrival(processes):
+    return sorted(processes, key=lambda x: x.AT)
 
-    def add_job(self, job):
-        self.queue.append(job)
+def sortByPriority(processes):
+    return sorted(processes, key=lambda x: x.priority)
 
-    def fcfs(self):
-        self.queue.sort(key=lambda job: job.arrival_time)
+class FCFS:
+    def execute(self, processes):
+        processes.sort(key=lambda x: x.AT)
 
         total_waiting_time = 0
-        for job in self.queue:
-            total_waiting_time += self.time - job.arrival_time
-            self.execute_job(job)
-        self.avg_waiting_time = total_waiting_time / len(self.queue)
-        self.calculate_total_burst_time()
+        for process in processes:
+            total_waiting_time += process.CT - process.AT
+            process.CT += process.BT
+            process.TAT = process.CT - process.AT
+            process.WT = process.TAT - process.BT
+            if process.WT < 0:
+                process.WT = 0
+            process.display()
 
-     def sjf_preemptive(self):
-        temp_queue = self.queue[:]
-        temp_queue.sort(key=lambda job: (job.arrival_time, job.burst_time))
+        avg_waiting_time = total_waiting_time / len(processes)
+        avg_TAT = sum(process.TAT for process in processes) / len(processes)
+        print(f"Average Waiting Time: {avg_waiting_time}")
+        print(f"Average TAT: {avg_TAT}")
+
+class SJF:
+    def execute(self, processes):
+        processes.sort(key=lambda x: (x.AT, x.remBT))
+
+        time = 0
+        total_waiting_time = 0
+        count = 0
+        min_remBT = float('inf')
+
+        print("\n\nPRNo\tBT\tAT\tCT\tTAT\tWT")
+        print("============================================================================================")
+
+        while count < len(processes):
+            for i, process in enumerate(processes):
+                if process.AT <= time and 0 < process.remBT < min_remBT:
+                    min_remBT = process.remBT
+                    shortest = i
+
+            if min_remBT == float('inf'):
+                time += 1
+                continue
+
+            processes[shortest].remBT -= 1
+            min_remBT = processes[shortest].remBT
+
+            if min_remBT == 0:
+                min_remBT = float('inf')
+                count += 1
+                sum_time = time + 1
+                processes[shortest].CT = sum_time
+                processes[shortest].TAT = processes[shortest].CT - processes[shortest].AT
+                processes[shortest].WT = processes[shortest].TAT - processes[shortest].BT
+
+                total_waiting_time += processes[shortest].WT
+                processes[shortest].display()
+                
+            time += 1
+
+        avg_waiting_time = total_waiting_time / len(processes)
+        avg_TAT = sum(process.TAT for process in processes) / len(processes)
+        print(f"Average Waiting Time: {avg_waiting_time}")
+        print(f"Average TAT: {avg_TAT}")
+
+class PriorityNonPreemptive:
+    def execute(self, processes):
+        processes.sort(key=lambda x: x.priority)
 
         total_waiting_time = 0
-        while temp_queue:
-            job = temp_queue[0]
-            temp_queue = temp_queue[1:]
+        print("\n\nPRNo\tBT\tAT\tCT\tTAT\tWT\tPR")
+        print("============================================================================================")
 
-            if job.arrival_time > self.time:
-                self.time = job.arrival_time
+        for process in processes:
+            process.CT += process.BT
+            process.TAT = process.CT - process.AT
+            process.WT = process.TAT - process.BT
 
-            time_executed = min(job.remaining_time, temp_queue[0].arrival_time - self.time) if temp_queue else job.remaining_time
-            self.execute_job(job, time_executed)
-            total_waiting_time += self.time - job.arrival_time
+            total_waiting_time += process.WT
+            process.display()
 
-            if job.remaining_time > 0:
-                temp_queue.append(job)
+        avg_waiting_time = total_waiting_time / len(processes)
+        avg_TAT = sum(process.TAT for process in processes) / len(processes)
+        print(f"Average Waiting Time: {avg_waiting_time}")
+        print(f"Average TAT: {avg_TAT}")
 
-        num_completed_jobs = len(self.queue) - len(temp_queue)
-        self.avg_waiting_time = total_waiting_time / (num_completed_jobs if num_completed_jobs > 0 else 1)
-        self.calculate_total_burst_time()
+class RoundRobin:
+    def execute(self, processes, quantum):
+        processes.sort(key=lambda x: x.AT)
 
-    def priority_non_preemptive(self):
-        self.queue.sort(key=lambda job: job.arrival_time)
-
-        total_waiting_time = 0
-        for job in self.queue:
-            total_waiting_time += self.time - job.arrival_time
-            self.execute_job(job)
-        self.avg_waiting_time = total_waiting_time / len(self.queue)
-        self.calculate_total_burst_time()
-
-    def round_robin_preemptive(self, time_slice):
-        temp_queue = self.queue[:]
+        time = 0
         total_waiting_time = 0
 
-        while temp_queue:
-            job = temp_queue[0]
-            temp_queue = temp_queue[1:]
-            waiting_time = self.time - job.arrival_time
-            total_waiting_time += waiting_time
-            time_executed = min(time_slice, job.remaining_time)
-            job.remaining_time -= time_executed
-            self.time += time_executed
+        print("\n\nPRNo\tBT\tAT\tCT\tTAT\tWT\tPR")
+        print("============================================================================================")
 
-            if job.remaining_time > 0:
-                temp_queue.append(job)
-            else:
-                self.print_job_completion(job)
+        while True:
+            done = True
+            for process in processes:
+                if 0 < process.remBT and process.AT <= time:
+                    done = False
 
-        num_completed_jobs = len(self.queue) - len(temp_queue)
-        self.avg_waiting_time = total_waiting_time / (num_completed_jobs if num_completed_jobs > 0 else 1)
-        self.calculate_total_burst_time()
+                    if process.remBT > quantum:
+                        time += quantum
+                        process.remBT -= quantum
+                        print(f"Running Process {process.name} from {time - quantum} to {time}")
+                    else:
+                        time += process.remBT
+                        print(f"Running Process {process.name} from {time - process.remBT} to {time}")
 
-    def execute_job(self, job, time_executed=None):
-        if time_executed is not None:
-            self.time += time_executed
-            job.remaining_time -= time_executed
-            self.print_job_execution(job, time_executed)
-        else:
-            self.time = max(self.time, job.arrival_time)
-            self.print_job_execution(job)
-            self.time += job.burst_time
+                        process.remBT = 0
+                        process.CT = time
+                        process.TAT = process.CT - process.AT
+                        process.WT = process.TAT - process.BT
+                        total_waiting_time += process.WT
+                        print(f"Completed Process {process.name}")
+                        process.display()
 
-    def print_job_execution(self, job):
-        print(f"Time={self.time}, Executing {job}")
+            if done:
+                break
 
-    def print_job_completion(self, job):
-        print(f"Time={self.time}, Job {job.job_id} completed!")
-
-    def calculate_total_burst_time(self):
-        self.total_burst_time = sum(job.burst_time for job in self.queue)
+        avg_waiting_time = total_waiting_time / len(processes)
+        avg_TAT = sum(process.TAT for process in processes) / len(processes)
+        print(f"Average Waiting Time: {avg_waiting_time}")
+        print(f"Average TAT: {avg_TAT}")
 
 def main():
-    job1 = Job(1, 0, 8, 2)
-    job2 = Job(2, 1, 4, 1)
-    job3 = Job(3, 2, 9, 3)
-    job4 = Job(4, 3, 5, 4)
+    processes = []
+    ch = 0
 
-    scheduler = Scheduler()
-    scheduler.add_job(job1)
-    scheduler.add_job(job2)
-    scheduler.add_job(job3)
-    scheduler.add_job(job4)
+    while ch != -1:
+        print("CPU SCHEDULING ALGORITHMS")
+        print("\n 1.FCFS\n 2.SJF\n 3.PriorityNonPreemptive\n 4.Round Robin\n-1.Exit\nENTER: ")
+        ch = int(input())
 
-    print("FCFS:")
-    scheduler.fcfs()
-    print(f"Average Waiting Time: {scheduler.avg_waiting_time}")
-    print(f"Total Burst Time: {scheduler.total_burst_time}")
-
-    print("\nSJF Preemptive:")
-    scheduler.sjf_preemptive()
-    print(f"Average Waiting Time: {scheduler.avg_waiting_time}")
-    print(f"Total Burst Time: {scheduler.total_burst_time}")
-
-    print("\nPriority Non-Preemptive:")
-    scheduler.priority_non_preemptive()
-    print(f"Average Waiting Time: {scheduler.avg_waiting_time}")
-    print(f"Total Burst Time: {scheduler.total_burst_time}")
-
-    print("\nRound Robin Preemptive:")
-    scheduler.round_robin_preemptive(3)
-    print(f"Average Waiting Time: {scheduler.avg_waiting_time}")
-    print(f"Total Burst Time: {scheduler.total_burst_time}")
+        try:
+            if ch == 1:
+                fcfs = FCFS()
+                fcfs.execute(processes)
+            elif ch == 2:
+                sjf = SJF()
+                sjf.execute(processes)
+            elif ch == 3:
+                pr = PriorityNonPreemptive()
+                pr.execute(processes)
+            elif ch == 4:
+                rr = RoundRobin()
+                quantum = int(input("Enter Quantum Time: "))
+                rr.execute(processes, quantum)
+            elif ch == -1:
+                break
+            else:
+                raise ValueError("Unexpected value: " + str(ch))
+        except Exception as e:
+            print(f"Unexpected Value: {e}")
 
 if __name__ == "__main__":
     main()
